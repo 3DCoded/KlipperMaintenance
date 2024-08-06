d73e87e51dd96061b0eb13274ddac9c90b4f3be5
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-08-06 15:28:31 -0400

    add experimental API endpoint

--- maintain.py
+++ maintain.py
@@ -16,6 +16,9 @@ class Maintain:
         self.threshold = config.getint('threshold')
         self.message = config.get('message')
 
+        self.server.register_endpoint(
+            '/server/testcode', ['GET'], self.test_gcode)
+
     async def test_gcode(self):
         klippy_apis = self.server.lookup_component('klippy_apis')
         result = await klippy_apis.run_gcode('M117 hello!')
