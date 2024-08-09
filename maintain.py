d8da071ed67c08910aa3b1eaaabd999e3b29c1a6
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-08-08 20:08:38 -0400

    update maintain.py

--- maintain.py
+++ maintain.py
@@ -1,25 +1,52 @@
+from pathlib import Path
+
+CONFIG_DIR = Path.home() / 'printer_data' / 'config'
+
+
+class ConfigError(Exception):
+    pass
+
+
 class Maintain:
     def __init__(self, config):
         self.server = config.get_server()
         self.name = config.get_name()
 
-        _ = config.get('label', default='', deprecate=True)
+        self.config_path = CONFIG_DIR / \
+            config.get('config_path', 'maintain.conf')
 
-        self.trigger = config.getchoice(
-            option='trigger',
-            choices=[
-                'print_time',
-                'filament',
-                'time'
-            ]
-        )
-        self.threshold = config.getint('threshold')
-        self.message = config.get('message')
+        self.items = []
 
         self.server.register_endpoint(
-            '/server/testcode', ['GET'], self.test_gcode)
+            '/server/maintain/settings', ['GET'], self.maintain_settings)
 
-    async def test_gcode(self):
+    async def run_gcode(self, gcode):
         klippy_apis = self.server.lookup_component('klippy_apis')
-        result = await klippy_apis.run_gcode('M117 hello!')
-        return f'hi{result}'
+        for line in gcode:
+            await klippy_apis.run_gcode(line)
+
+    async def maintain_settings(self, request):
+        result = []
+        for item in self.items:
+            result.append({
+                'name': item.name,
+                'trigger': item.trigger,
+                'threshold': item.threshold,
+                'message': item.message
+            })
+        return result
+
+
+class MaintainItem:
+    def __init__(self, config, section):
+        self.trigger = config.get(section, 'trigger', fallback='print_time')
+        if self.trigger not in ['print_time', 'filament', 'time']:
+            raise ConfigError('Invalid trigger type')
+        self.threshold = config.getint(section, 'threshold')
+        if self.threshold <= 0:
+            raise ConfigError('Invalid threshold')
+        self.message = config.get(section, 'message', fallback='No message')
+
+
+def load_component(config):
+    return Maintain(config)
